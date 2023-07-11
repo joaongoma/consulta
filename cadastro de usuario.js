0d4cd80d70e2d4203455e78bc917348710424268
Your Name <you@example.com>
2023-07-11 20:12:51 -0300

    ficheiro js junto com codigo criado

---
+++ cadastro de usuario.js
@@ -0,0 +1,24 @@
+
+import "./App.css";
+import{useState} from "react";
+
+function App () {
+    const[ isVisible, setIsVisible] = useState(false);
+
+    return(
+
+        <div className="App">
+
+            <button onClick = {((setIsVisible((prev) =>!prev)))}>
+
+            {isVisible ? "Hide" : "Show"}
+
+            </button>
+
+            {isVisible && <h1> Hidden text </h1>}
+
+        </div>
+    );
+
+}
+export default App;
